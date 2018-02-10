37077ee0f562ccb4ea8b6265eea98b3a089c3c22
Hasika Mahtta <hasikaa.11@gmail.com>
2018-02-10 18:16:01 -0500

    Longest_substring

---
+++ Longest_substring.py
@@ -0,0 +1,23 @@
+from itertools import permutations
+def lengthOfLongestSubstring(s):
+    perms = []
+    list=[]
+    dict={'char':0,'freq':0}
+    for i in range(1, len(s) + 1):
+        for c in permutations(s, i):
+            perms.append("".join(c))
+    for j in range(len(perms)):
+        if perms[j] in s:
+            list.append(perms[j])
+    for each in list:
+        dict['char']=set(each)
+        dict['freq']=len(each)
+    max = -float('inf')
+    for val in dict.itervalues():
+        if val > max:
+            max = val
+    print len(max)
+
+
+
+lengthOfLongestSubstring("abcab")
