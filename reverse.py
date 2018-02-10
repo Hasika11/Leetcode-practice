c39c0ea8b5c8ac9d1f1a45ac7945ebf4ee123cc4
Hasika Mahtta <hasikaa.11@gmail.com>
2018-02-10 18:10:47 -0500

    Reversing the integer

---
+++ reverse.py
@@ -0,0 +1,21 @@
+def reverse(x):
+        s = str(x)
+        if s[0] == '-':
+            s1 = s[:0:-1]
+            reverse1=-int(s1)
+            if reverse1>2147483648 or reverse1<-2147483648:  #checking for 32 bit signed integer range
+                return 0
+            else:
+                return reverse1
+        else:
+            s1 = s[::-1]
+            reverse1= int(s1)
+            print 'r',reverse1
+            if reverse1>2147483648 or reverse1<-2147483648:     #checking for 32 bit signed integer range
+                return 0
+            else:
+                return reverse1
+
+
+
+
